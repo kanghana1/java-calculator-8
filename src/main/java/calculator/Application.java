77c3dd73e9fd9e87bee49dab8cf16d54c92a385a
kanghana1 <qazplm12093@naver.com>
2025-10-17 12:54:26 +0900

    feat: 기본 구분자 파싱 기능 구현

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -28,4 +28,51 @@ public class Application {
         return sum;
     }
 
+    private static List<Integer> parsing(String input) {
+        if (!input.contains("//")) {
+            return basicParsing(input);
+        } else {
+            return customParsing(input);
+        }
+    }
+
+    private static List<Integer> customParsing(String input) {
+
+        return null;
+    }
+
+    private static List<Integer> basicParsing(String input) {
+        List<Integer> results = new ArrayList<>();
+        String[] parsingToComma;
+        String[] parsingToColon;
+
+        boolean containsComma = input.contains(",");
+        boolean containsColon = input.contains(":");
+
+        if (containsComma) {
+            parsingToComma = input.split(",");
+
+            // 1. ,와 :이 둘 다 있는 경우
+            if (containsColon) {
+                for (String str : parsingToComma) {
+                    parsingToColon = str.split(":");
+
+                    for (String fin : parsingToColon) {
+                        results.add(Integer.valueOf(fin));
+                    }
+                }
+            } else { // 2. ,만 있는 경우
+                for (String fin : parsingToComma) {
+                    results.add(Integer.valueOf(fin));
+                }
+            }
+        } else if (containsColon) { // 3. :만 있는 경우
+            parsingToColon = input.split(":");
+            for (String fin : parsingToColon) {
+                results.add(Integer.valueOf(fin));
+            }
+        }
+
+        return results;
+    }
 }
